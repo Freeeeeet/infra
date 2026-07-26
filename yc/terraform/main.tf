ea645c9c30641857215ebdb6685d14aa7c42557f
bodya <bodya>
2026-07-26 14:03:39 +0300

    less

--- yc/terraform/main.tf
+++ yc/terraform/main.tf
@@ -19,23 +19,23 @@ module "toronto" {
   image_id           = var.image_id
   disk_size          = 50
   disk_type          = var.disk_type
-  cores              = 4
-  memory             = 8
+  cores              = 2
+  memory             = 4
   user_data          = file("${path.module}/cloud_ssh_user_data")
   security_group_ids = [yandex_vpc_security_group.web.id]
   nat_ip_address     = yandex_vpc_address.toronto.external_ipv4_address[0].address
   disk_auto_delete   = false
 }
 
-module "winnipeg" {
-  source             = "./modules/vm"
-  name               = "winnipeg"
-  subnet_id          = yandex_vpc_subnet.default_subnet.id
-  image_id           = var.image_id
-  disk_size          = var.disk_size
-  disk_type          = var.disk_type
-  cores              = var.cores
-  memory             = var.memory
-  user_data          = file("${path.module}/cloud_ssh_user_data")
-  security_group_ids = [yandex_vpc_security_group.web.id]
-}
+# module "winnipeg" {
+#   source             = "./modules/vm"
+#   name               = "winnipeg"
+#   subnet_id          = yandex_vpc_subnet.default_subnet.id
+#   image_id           = var.image_id
+#   disk_size          = var.disk_size
+#   disk_type          = var.disk_type
+#   cores              = var.cores
+#   memory             = var.memory
+#   user_data          = file("${path.module}/cloud_ssh_user_data")
+#   security_group_ids = [yandex_vpc_security_group.web.id]
+# }
